fd594427f844106792bce8ec0c82db1ef1793299
Lee Roy Akbar <leeroyakbar@gmail.com>
2024-04-24 14:51:31 +0700

    add products list

--- App.js
+++ App.js
@@ -1,77 +1,149 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View, StyleSheet, Image } from 'react-native';
-const imgPerson = require('./assets/person.png')
+import { Button, StyleSheet, Text, View, Pressable, Image, ScrollView } from 'react-native'
+import React, { useState } from 'react'
+import HomeScreen from './app/screen/HomeScreen/HomeScreen.js'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
-
+const data = [
+  {
+    id: 1,
+    name: "F150",
+    price: 500000000,
+    stock: 1,
+    url: "https://www.butlertire.com/images/trending/2018/9/21/Ford_Raptor_with_22in_Grid_Off-road_GF2_Wheels_and_Toyo_Open_Country_MT_Tires_and_4in_BDS_Lift_12018921_large.jpeg"
+  },
+  {
+    id: 2,
+    name: "Civic",
+    price: 300000000,
+    stock: 10,
+    url: "https://cdn.carshowroom.com.au/media/21488027/2018-honda-civic-type-r-fk8-39.jpg"
+  },
+  {
+    id: 3,
+    name: "Hilux",
+    price: 500000000,
+    stock: 7,
+    url: "https://paultan.org/image/2021/09/Toyota-Hilux-Rad-widebody-5-BM-630x354.jpeg"
+  },
+  {
+    id: 4,
+    name: "Triton",
+    price: 300000000,
+    stock: 13,
+    url: "https://img-ik.cars.co.za/news-site-za/images/2020/06/Mitsubishi-TritonXtreme-3.jpg?tr=w-1200,h-800"
+  },
+  {
+    id: 5,
+    name: "Palisade",
+    price: 100000000,
+    stock: 15,
+    url: "https://cdni.autocarindia.com/ExtraImages/20220414061536_Hyundai_Palisade_facelift_front.jpg"
+  }
+]
 
 export default function App() {
+
+  const[count, setCount] = useState(0)
+  const[products, setProducts] = useState(data)
+
+  const handleAdd = () => {
+    setCount(count + 1)
+  }
+
+  const handleMin = () => {
+    if(count <= 0) return;
+    setCount(count -1)
+  }
+
   return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.textHello}>Hello</Text>
-        <Text style={styles.textName}>Lee Roy Akbar</Text>
+    <SafeAreaView style={styles.container}>
+      <View style= {{flex: 1}}>
+        <HomeScreen/>
       </View>
-      <View style={styles.infoSection}>
-        <View>
-          <Text style={styles.infoText}>Todo Done</Text>
-          <Text style={styles.nextInfoText}>Keep it up</Text>
-        </View>
-        <View style={styles.circleInfo}>
-          <Text style={styles.circleText}>0/1</Text>
-        </View>
+      <View style={styles.buttonContainer}>
+        <Pressable style={styles.buttonAdd} onPress={handleMin}>
+          <Text style={styles.textButton}>Add</Text>
+        </Pressable>
+        <Text style={styles.textCount}>{count}</Text>
+        <Pressable style={styles.buttonAdd} onPress={handleAdd}>
+          <Text style={styles.textButton}>Add</Text>
+        </Pressable>
       </View>
-    </View>
-  );
+
+      <ScrollView style={styles.listContainer}>
+        {products.map((product) => {
+          return(
+            <View key={product.id} style={styles.productItems}>
+                <Image
+                  source={{uri : `${product.url}`}}
+                  style={styles.productImages}
+                />
+              <View style={styles.description}>
+                <Text style={{fontSize: 18, fontWeight: "bold"}}>Name: {product.name}</Text>
+                <Text style={{fontSize: 18, fontWeight: "bold"}}>${product.price}</Text>
+                <Text>{product.stock} Units</Text>
+              </View>
+            </View>
+          )
+        } )}
+      </ScrollView>
+    </SafeAreaView>
+  )
 }
 
-
 const styles = StyleSheet.create({
-  container: {
+  container : {
     flex: 1,
-    paddingTop: 10,
+    backgroundColor: "#fffffe",
+    paddingTop: 24
   },
-  header: {
-    marginLeft: 20,
-    marginTop: 30,
-    alignItems: 'left',
-    justifyContent: 'center',
+  buttonContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 30,
   },
-  textHello: {
-    fontSize: 20,
-  },
-  textName: {
-    fontSize: 30,
-    fontWeight: 'bold',
-  },
-  infoSection: {
-    borderRadius: 8,
-    padding: 30,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    paddingHorizontal: 30,
+  buttonAdd: {
+    paddingHorizontal: 20,
     paddingVertical: 10,
-    backgroundColor: '#004169',
-    margin: 20
+    backgroundColor: "#5BBCFF",
+    alignItems: "center",
+    justifyContent: "center",
+    color: "#fffffe",
+    borderRadius: 5,
+    
   },
-  infoText:{
-    fontSize: 20,
-    color: 'white'
-  },
-  nextInfoText: {
+  textButton: {
+    color: "#fffffe",
     fontSize: 15,
-    color: 'white'
   },
-  circleInfo: {
-    width: 100,
-    height: 100,
-    borderRadius: 50,
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
+  textCount: {
+    fontSize: 30,
+    paddingHorizontal: 30,
   },
-  circleText: {
-    fontSize: 20,
-    fontWeight: 'bold'
+  listContainer: {
+    flex: 1,
+    paddingHorizontal: 8,
+  },
+  productItems: {
+    flexDirection: "row",
+    justifyContent: 'space-around',
+    backgroundColor: "#5BBCFF",
+    borderWidth: 1,
+    marginBottom: 12,
+    
+  },
+  productImages:{
+    width: 200,
+    height: 150,
+    borderRadius: 10,
+    marginVertical: 20,
+    borderWidth: 4,
+    padding: 4,
+    borderColor: "#FFD1E3",
+    
+  },
+  description: {
+    justifyContent: 'center'
   }
-});
+})
---
+++ app/screen/HomeScreen/HomeScreen.js
@@ -0,0 +1,16 @@
+import { ScrollView, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function HomeScreen() {
+  return (
+    <View>
+        <ScrollView>
+             <Text>HomeScreen</Text>
+        </ScrollView>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+
+})
